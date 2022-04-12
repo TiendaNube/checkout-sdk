468d668ba30740dcef578736061dafeb03cf794e
wellingtonfjr <wellingtonfjr@hotmail.com>
2022-04-12 16:22:10 -0300

    Removed console log

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import PubSub from 'pubsub-js'
 
-console.log('start Checkout SDK')
 const setInitialWindowSDK = () => {
-  console.log('window=>', window)
   window.SDKCheckout = {
     publishEvent: (event, data) => PubSub.publish(event, data),
     publishEventSync: (event, data) => PubSub.publishSync(event, data),
